5d4b61bca75fb759e5bb463bb20f7982e47d6939
Charan25-5 <charan25.b@gmail.com>
2026-02-11 17:52:41 +0530

    Section 3

---
+++ client/src/app/layout/App.tsx
@@ -0,0 +1,46 @@
+import { useState } from "react"
+import { Container, createTheme, ThemeProvider, Box, CssBaseline } from '@mui/material';
+import Navbar from "./navbar";
+import { Outlet } from "react-router-dom";
+
+function App() {
+
+  const[darkMode,setDarkMode] = useState(false);
+  const palleteType = darkMode ? 'dark' : 'light'
+
+  const theme = createTheme({
+    palette:{
+      mode:palleteType,
+      background:{
+        default:(palleteType === 'light') ? '#eaeaea' : '#121212'
+      }
+    }
+  })
+
+  const toggleDarkMode = ()=>{
+    setDarkMode(!darkMode);
+  }
+
+    return (
+      <ThemeProvider theme={theme}>
+        
+        <CssBaseline/>
+        <Navbar toggleDarkMode={toggleDarkMode} darkMode={darkMode}/>
+        <Box
+        sx={{
+          minHeight:'100vh',
+          background:darkMode 
+            ? 'radial-gradient(circle,#1e3aBa,#111B27)' 
+            : 'radial-gradient(circle,#baecf9,#f0f9ff)', 
+            py:6
+        }}>
+          <Container maxWidth='xl' sx={{mt:8}}>
+          <Outlet/>
+          </Container>
+        </Box>
+        
+      </ThemeProvider>
+  )
+}
+
+export default App
---
+++ client/src/features/catalog/Catalog.tsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from 'react';
+import type { Product } from '../../app/models/product';
+import ProductList from './ProductList';
+
+
+
+export default function Catalog() {
+
+  const[products,setProducts] = useState<Product[]>([]);
+
+  useEffect(()=>{
+    fetch('https://localhost:7280/api/products')
+    .then(response => response.json())
+    .then(data => setProducts(data));
+  },[])  //wepage reload   
+    
+
+  return (
+    <>
+      <ProductList products={products}/>
+    </>
+  )
+}
